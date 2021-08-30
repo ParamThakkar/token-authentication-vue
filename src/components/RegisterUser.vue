9150130583e1dc1eb48a09e1a288afe3cb34d27e
Param Thakkar <param.thakkar@ftxinfotech.com>
2021-08-30 13:35:05 +0530

    Initial commmit

---
+++ src/components/RegisterUser.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <form
+      autocomplete="nope"
+      @submit.prevent="register"
+    >
+      <label for="name">Name:</label>
+      <input
+        v-model="name"
+        type="text"
+        name="name"
+        autocomplete="nope"
+        value
+      >
+      <label for="email">Email:</label>
+      <input
+        v-model="email"
+        type="email"
+        name="email"
+        autocomplete="nope"
+        value
+      >
+      <label for="password">Password:</label>
+      <input
+        v-model="password"
+        type="password"
+        name="password"
+        autocomplete="nope"
+        value
+      >
+
+      <button
+        type="submit"
+        name="button"
+      >
+        Register
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: '',
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    register () {
+      this.$store.dispatch('register', {
+        name: this.name,
+        email: this.email,
+        password: this.password
+      })
+        .then(() => {
+          this.$router.push({ name: 'dashboard' })
+        })
+    }
+  }
+}
+</script>
